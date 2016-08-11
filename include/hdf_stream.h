bc636554455157426dce5d5ac2bca1a45d530a85
Igor Babuschkin <igor@babuschk.in>
2016-08-11 19:06:33 +0100

    Add dummy hdf stream implementation

--- include/hdf_stream.h
+++ include/hdf_stream.h
@@ -5,7 +5,6 @@
 #include <google/protobuf/io/zero_copy_stream.h>
 #include <google/protobuf/io/coded_stream.h>
 
-
 namespace laminate {
 
 using google::protobuf::int64;
--- src/hdf_stream.cc
+++ src/hdf_stream.cc
@@ -11,8 +11,40 @@ HDFOutputStream::HDFOutputStream(ZeroCopyOutputStream *output,
   : output_(output) {
 };
 
+int64 HDFOutputStream::ByteCount() const {
+  return 0;
+}
+
+void HDFOutputStream::BackUp(int count) {
+}
+
+bool HDFOutputStream::Next(void** data, int* size) {
+  return false;
+}
+
+HDFOutputStream::~HDFOutputStream() {
+}
+
 HDFInputStream::HDFInputStream(ZeroCopyInputStream *input)
   : input_(input) {
-};
+}
+
+HDFInputStream::~HDFInputStream() {
+}
+
+int64 HDFInputStream::ByteCount() const {
+  return 0;
+}
+
+bool HDFInputStream::Skip(int count) {
+  return false;
+}
+
+bool HDFInputStream::Next(const void** data, int* size) {
+  return false;
+}
+
+void HDFInputStream::BackUp(int count) {
+}
 
 }
