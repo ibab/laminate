ad28bcb1ac0f13f6827b5b5c4d5a14faf81b5e81
Igor Babuschkin <igor@babuschk.in>
2016-08-19 18:39:35 +0100

    Fix ci build

--- examples/column_store.cc
+++ examples/column_store.cc
@@ -1,12 +1,11 @@
 
 #include "column_store.h"
-#include <iostream>
 #include "examples/example.pb.h"
 
 using namespace example;
 
 int main() {
   Person person;
-  auto meta = laminate::ColumnWriter::Create(person);
+
   return 0;
 }
